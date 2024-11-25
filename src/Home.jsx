30aee6c19ae90ee05e1194b965378916a974dedc
myca05 <carrillojecel@gmail.com>
2024-11-25 16:23:16 +0800

    Members

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,12 @@ const HomePage = () => {
       <div style={styles.content}>
         <h1 style={styles.homeText}>Home</h1>
         <h1>LGU-SB-Management-Platform</h1>
+        <h2>Members</h2>
+        <h3>Jecel Carrillo</h3>
+        <h3>Steven De Los Reyes</h3>
+        <h3>Jorge Nerosa II</h3>
+        <h3>John Myca Arriezza</h3>
+        <h3>James Ryan Cejas</h3>
       </div>
     </div>
   );
